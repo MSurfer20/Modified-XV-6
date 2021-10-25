fde48b03c19a507f5411b95fb7668b189c56df40
Suyash Vardhan Mathur <suyash.mathur@research.iiit.ac.in>
2021-10-25 13:11:31 +0530

    add user part for setpriority.

---
+++ user/setpriority.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int i;
+
+  if(argc < 3){
+    fprintf(2, "Usage: setpriority priority pid\n");
+    exit(1);
+  }
+  int priority, pid;
+  priority=atoi(argv[1]);
+  pid=atoi(argv[2]);
+  setpriority(priority, pid);
+  exit(0);
+}
--- user/user.h
+++ user/user.h
@@ -24,6 +24,7 @@ char* sbrk(int);
 int sleep(int);
 int uptime(void);
 int trace(int);
+int setpriority(int, int);
 
 // ulib.c
 int stat(const char*, struct stat*);
